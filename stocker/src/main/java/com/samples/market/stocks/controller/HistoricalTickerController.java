96791311d08f05713b5048e1f2934b2accd09b50
chiusday <chiusday@hotmail.sg>
2019-03-25 10:32:57 +0800

    initial stocker cloud commit

---
+++ stocker/src/main/java/com/samples/market/stocks/controller/HistoricalTickerController.java
@@ -0,0 +1,25 @@
+package com.samples.market.stocks.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.samples.market.model.GetBySymbolRequest;
+import com.samples.market.stocks.service.HistoricalTickerService;
+import com.samples.market.stocks.visitor.model.HistoricalTickerListVisitorModel;
+
+@RestController
+public class HistoricalTickerController {
+	@Autowired
+	private HistoricalTickerService historicalTickerService;
+	
+	@PostMapping("/stock/list")
+	public ResponseEntity<Object> getJson(@RequestBody GetBySymbolRequest request){
+		HistoricalTickerListVisitorModel historicalTickerVisitorModel = 
+				historicalTickerService.getHistoricalTickers(request.getSymbol()); 
+		
+		return historicalTickerVisitorModel.getResponseEntity();
+	}
+}
